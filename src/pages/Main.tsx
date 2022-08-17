f5628f2774c93fae526022216bbea443cfe47263
bruh-git <brunac.cardososilva@gmail.com>
2022-08-17 11:53:23 -0300

    main structure in progress

---
+++ src/pages/Main.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import Frontend from '../templates/Frontend';
+
+function Main() {
+  return (
+    <div>
+      <Frontend>Template</Frontend>
+    </div>
+  )
+}
+
+export default Main;
